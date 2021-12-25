2678df2a6c71d87f472940b9e1e74df2f627b636
reeham_raed <rolla.ghanima@gmail.com>
2021-12-25 14:15:27 +0200

    generate data

---
+++ data/index.js
@@ -0,0 +1,32 @@
+export const data = [
+	{
+		"name": "Deacon Francis",
+		"id": "64CDE4AC-74DB-4859-A96C-3B9A12EE61B1",
+		"email": "vulputate@outlook.edu",
+		"phone": "1-435-315-4821"
+	},
+	{
+		"name": "Sebastian Little",
+		"id": "F64B955C-7ADE-7CAD-1B28-776918913552",
+		"email": "lobortis.augue@aol.edu",
+		"phone": "(658) 336-6758"
+	},
+	{
+		"name": "Brooke Banks",
+		"id": "DD2965CE-CBBA-3ECE-8578-E87B67699D27",
+		"email": "arcu.imperdiet@aol.ca",
+		"phone": "1-201-802-4957"
+	},
+	{
+		"name": "Timothy Wheeler",
+		"id": "64B11D54-2482-427E-24E8-BC1DF41F7B42",
+		"email": "duis@google.couk",
+		"phone": "1-110-231-4164"
+	},
+	{
+		"name": "Cain Ray",
+		"id": "F2BA005A-C568-B669-54C4-A4D9FEFEB7FD",
+		"email": "vitae@yahoo.couk",
+		"phone": "1-173-161-3059"
+	}
+];
